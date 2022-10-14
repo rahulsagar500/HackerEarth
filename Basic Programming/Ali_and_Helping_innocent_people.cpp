1549c5e2c3803a13cac202ebb9c1da844982c228
RAHUL SAGAR PABBA <100937627+rahulsagar500@users.noreply.github.com>
2022-10-14 22:11:55 +0530

    Update Ali_and_Helping_innocent_people.cpp
    Commiting after adding the comments

--- Basic Programming/Ali_and_Helping_innocent_people.cpp
+++ Basic Programming/Ali_and_Helping_innocent_people.cpp
@@ -8,14 +8,14 @@ int main(){
     string c=a.substr(3,2);
     string d=a.substr(7,2);
     string h=a.substr(4,2);
-    int e=stoi(b);
+    int e=stoi(b);                  // converting the string to int
     int f=stoi(c);
     int g=stoi(d);
     int i=stoi(h);
-    if(a[2]=='A' || a[2]=='E' || a[2]=='I' || a[2]=='O' || a[2]=='U' || a[2]=='Y'){
+    if(a[2]=='A' || a[2]=='E' || a[2]=='I' || a[2]=='O' || a[2]=='U' || a[2]=='Y'){             // condition for not being a vovel which included Y
         cout<<"invalid";
     }
-    else if ((e/10+e%10)%2==0 && (f/10+f%10)%2==0 && (g/10+g%10)%2==0 && (i/10+i%10)%2==0)
+    else if ((e/10+e%10)%2==0 && (f/10+f%10)%2==0 && (g/10+g%10)%2==0 && (i/10+i%10)%2==0)     //sum of the consecutive integers
     {
         cout<<"valid";
     }
