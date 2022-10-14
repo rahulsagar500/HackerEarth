2f9dc60a59cf6bd8c93c07e952f2cc2e97fe7882
RAHUL SAGAR PABBA <100937627+rahulsagar500@users.noreply.github.com>
2022-10-14 22:01:06 +0530

    Add files via upload
    Adding the code for Ali helping innocent people in C++

---
+++ Basic Programming/Ali_and_Helping_innocent_people.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a;
+    cin>>a;
+    string b=a.substr(0,2);
+    string c=a.substr(3,2);
+    string d=a.substr(7,2);
+    string h=a.substr(4,2);
+    int e=stoi(b);
+    int f=stoi(c);
+    int g=stoi(d);
+    int i=stoi(h);
+    if(a[2]=='A' || a[2]=='E' || a[2]=='I' || a[2]=='O' || a[2]=='U' || a[2]=='Y'){
+        cout<<"invalid";
+    }
+    else if ((e/10+e%10)%2==0 && (f/10+f%10)%2==0 && (g/10+g%10)%2==0 && (i/10+i%10)%2==0)
+    {
+        cout<<"valid";
+    }
+    else{
+        cout<<"invalid";
+    }
+    
+    return 0;
+}
