7785e887d0d5b4b112c261242000e6dc151cab17
RAHUL SAGAR PABBA <100937627+rahulsagar500@users.noreply.github.com>
2022-10-14 23:24:20 +0530

    Update palindromic_string.cpp
    After adding the comments

--- Basic Programming/palindromic_string.cpp
+++ Basic Programming/palindromic_string.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main(){
     string a;
-    string b="";
+    string b="";                                //initalising the empty string 
     cin>>a;
     for(int i=a.length()-1;i>=0;i=i-1){
-        b=b+a[i];
-    }
-    if(a==b){
-        cout<<"YES";
+        b=b+a[i];                              //adding the string from the last
+    } 
+    if(a==b){                                  
+        cout<<"YES";                            //condition for both the strings are same or not
     }
     else{
         cout<<"NO";
